2dcf3e5655cc567a35419faa6f0280be0946d415
raja <rajamr9187@gmail.com>
2024-06-21 21:21:45 +0400

    3rd day update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import {Suspense, lazy} from 'react';
-import Loader from './components/Loader';
-const Dashboard=lazy(()=>import("./pages/Dashboard"))
-const Product=lazy(()=>import("./pages/Product"))
-const Customer=lazy(()=>import("./pages/Customer"))
-const Transcations=lazy(()=>import("./pages/Transaction"))
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Product = lazy(() => import("./pages/Product"));
+const Customer = lazy(() => import("./pages/Customer"));
+const Transcations = lazy(() => import("./pages/Transaction"));
+
+const Newproduct = lazy(() => import("./pages/management/NewProduct"));
+
+const ProductManagement = lazy(() => import("./pages/management/ProductManagement"));
+
+const TranscationManagement = lazy(() => import("./pages/management/TranscationManagement"));
+
+
 function App() {
   return (
     <Router>
-      
-      <Suspense fallback={<Loader/>}>
-      <Routes>
-        <Route path="/admin/dashboard" element={<Dashboard/>}/>
-        <Route path="/admin/product" element={<Product/>}/>
-        <Route path="/admin/transcation" element={<Transcations/>}/>
-        <Route path="/admin/customer" element={<Customer/>}/>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Product />} />
+          <Route path="/admin/transcation" element={<Transcations />} />
+          <Route path="/admin/customer" element={<Customer />} />
+
+          {/* Charts */}
+          {/* Apps */}
+
+          {/* Management */}
+
+          <Route path="/admin/product/new" element={<Newproduct />} />
+          <Route path="/admin/product/:id" element={<ProductManagement/>} />
+          <Route path="/admin/transaction/:id" element={<TranscationManagement/>} />
+        </Routes>
       </Suspense>
-      
-       {/* Charts */}
-      {/* Apps */}
-      
     </Router>
-  )
+  );
 }
-export default App
+export default App;
---
+++ src/pages/management/TranscationManagement.tsx
@@ -0,0 +1,13 @@
+
+import Adminsidebar from '../../components/Adminsidebar';
+
+const TranscationManagement = () => {
+  return (
+    <div className="admin-container" >
+      <Adminsidebar/>
+      <main className="product-management">TRanscation Management</main>
+    </div>
+  )
+}
+
+export default TranscationManagement
